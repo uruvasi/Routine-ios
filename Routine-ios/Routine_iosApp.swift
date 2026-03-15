d6902e6f19f4bcdae2a5ff08d33ca634a0d205af
uruvasi <uruvasi@gmail.com>
2026-03-15 10:23:12 +0900

    Initial Commit

---
+++ Routine-ios/Routine_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Routine_iosApp.swift
+//  Routine-ios
+//
+//  Created by uruvasi on 2026/03/15.
+//
+
+import SwiftUI
+
+@main
+struct Routine_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
